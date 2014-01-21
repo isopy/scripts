fe52706eed0f99a1dfd185ac77a358eb09047dcb
Michael Johnson <mjohnson@redhat.com>
2014-01-21 14:13:56 -0500

    adding a new script

---
+++ scripts/versionmerge_all_modules.sh
@@ -0,0 +1,7 @@
+#Change ~/puppet-cfg/modules/* to reflect the path to your modules
+for a in ~/puppet-cfg/modules/*;
+do
+   cd $a
+   versionmerge-init
+   echo -e "The $a directory has been version-merged\n"
+done
