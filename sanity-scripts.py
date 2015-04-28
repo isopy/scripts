ccf1cc27a6e975c921792ef016abd917208ba8e2
Michael Johnson <mjohnson@redhat.com>
2015-04-28 17:33:00 -0400

    adding better output formatting / lets only write to one file

--- sanity-scripts.py
+++ sanity-scripts.py
@@ -2,16 +2,16 @@
 import subprocess
 
 #Create variables that contain the shell commands
-uptime = "uptime > /root/sanity-check_uptime.log_`date +%m%d%y-%T`"
-netstat = "netstat -nap > /root/sanity-check_netstat.log_`date +%m%d%y-%T`"
-mount = "mount > /root/sanity-check_mount.log_`date +%m%d%y-%T`"
-ps = "ps auxwwf > /root/sanity-check_ps.log_`date +%m%d%y-%T`"
-lsof = "lsof > /root/sanity-check_lsof.log_`date +%m%d%y-%T`"
-ifconfig = "ifconfig -a > /root/sanity-check_ifconfig.log_`date +%m%d%y-%T`"
-route = "route -n > /root/sanity-check_route.log_`date +%m%d%y-%T`"
-env = "env > /root/sanity-check_env.log_`date +%m%d%y-%T`"
-fstab = "cat /etc/fstab > /root/sanity-check_fstab.log_`date +%m%d%y-%T`"
-yum_log = "cat /var/log/yum.log > /root/sanity-check_yum.log_`date +%m%d%y-%T`"
+uptime = "uptime >> /root/sanity-checks.log_`date +%F`"
+netstat = "netstat -nap >> /root/sanity-checks.log_`date +%F`"
+mount = "mount >> /root/sanity-checks.log_`date +%F`"
+ps = "ps auxwwf >> /root/sanity-checks.log_`date +%F`"
+lsof = "lsof >> /root/sanity-checks.log_`date +%F`"
+ifconfig = "ifconfig -a >> /root/sanity-checks.log_`date +%F`"
+route = "route -n >> /root/sanity-checks.log_`date +%F`"
+env = "env >> /root/sanity-checks.log_`date +%F`"
+fstab = "cat /etc/fstab >> /root/sanity-checks.log_`date +%F`"
+yum_log = "cat /var/log/yum.log >> /root/sanity-checks.log_`date +%F`"
 
 #Places variables into a list/array
 cmds = [uptime, netstat, mount, ps, lsof, ifconfig, route, env, fstab, yum_log]
@@ -21,6 +21,9 @@ cmds = [uptime, netstat, mount, ps, lsof, ifconfig, route, env, fstab, yum_log]
 def runCommands(command=cmds):
     #Iterates over list, running statements for each item in the list
     for cmd in cmds:
+        subprocess.call('echo -e "####################" >> /root/sanity-checks.log_`date +%F`', shell=True)
         subprocess.call(cmd, shell=True)
+        subprocess.call('echo -e "####################" >> /root/sanity-checks.log_`date +%F`', shell=True)
+        subprocess.call('echo -e "\n" >> /root/sanity-checks.log_`date +%F`', shell=True)
 
 runCommands()
